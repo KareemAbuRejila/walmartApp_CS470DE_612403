6e1fef7dea3facc8b60e49aec39c99577c3b7e5a
Kareem AbuRijela <39139830+KareemAbuRejila@users.noreply.github.com>
2022-03-12 17:54:04 -0600

    create

---
+++ app/src/main/java/edu/miu/cs473de/walmartapp_cs470de_612403/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.miu.cs473de.walmartapp_cs470de_612403
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
